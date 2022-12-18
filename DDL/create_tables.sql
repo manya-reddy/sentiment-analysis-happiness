15d9d24cc70e239de2c5799f514f9a23f8d3b707
manya-reddy <113812925+manya-reddy@users.noreply.github.com>
2022-12-18 12:27:46 -0500

    updated

--- DDL/create_tables.sql
+++ DDL/create_tables.sql
@@ -1,51 +1,59 @@
-create database if not exists assignment_3;
-use assignment_3;
+create database if not exists assignment_3_new;
+use assignment_3_new;
 
 # Create tables
-create table if not exists country(
-country_code CHAR(5) primary key, country_name VARCHAR(25) );
+
+create table if not exists country (
+country_code CHAR(5) primary key,
+country_name VARCHAR(25) );
 
 create table if not exists crime(
-crime_id INT primary key auto_increment,
-country_code CHAR(5),
+country_code CHAR(5) primary key,
 crime_index DECIMAL(10,6),
 unemployment DECIMAL(10,6),
-foreign key (country_code) references country(country_code) );
+foreign key (country_code) references country(country_code)
+);
+
 
 create table if not exists alchappiness(
-alchappiness_id INT primary key auto_increment, country_code CHAR(5),
-human_development DECIMAL(10,6), beer_capita DECIMAL(10,6),
- wine_capita DECIMAl(10,6),
-foreign key (country_code) references country(country_code) );
+country_code CHAR(5) primary key,
+human_development DECIMAL(10,6),
+beer_capita DECIMAL(10,6),
+wine_capita DECIMAl(10,6),
+foreign key (country_code) references country(country_code)
+);
+
 
 create table if not exists gdp(
-gdp_id INT primary key auto_increment,
-country_code CHAR(5),
-year_2020 BIGINT(50),
-year_2021 BIGINT(50),
-foreign key (country_code) references country(country_code) );
+country_code CHAR(5) primary key,
+year_2020 BIGINT,
+year_2021 BIGINT,
+foreign key (country_code) references country(country_code)
+);
+
 
 create table if not exists happiness(
-happiness_id INT primary key auto_increment,
-country_code CHAR(5),
+country_code CHAR(5) primary key,
 freedom DECIMAL(10,6),
 happiness_score DECIMAL(10,6),
-foreign key (country_code) references country(country_code) );
+foreign key (country_code) references country(country_code)
+);
 
 create table if not exists internet(
-internet_id INT primary key auto_increment,
-country_code CHAR(5),
+country_code CHAR(5) primary key,
 income_person DECIMAL(20,10),
 internet_rate DECIMAL(20,10),
-foreign key (country_code) references country(country_code) );
+foreign key (country_code) references country(country_code)
+);
+
 
 create table if not exists literacy(
-literacy_id INT primary key auto_increment,
-country_code CHAR(5),
+country_code CHAR(5) primary key,
 data_year INT,
 literacy_rate DECIMAL(10,6),
-population BIGINT(55),
-foreign key (country_code) references country(country_code) );
+population BIGINT,
+foreign key (country_code) references country(country_code)
+);
 
 create table if not exists covidstatus(
 country_code CHAR(5) primary key,
@@ -54,7 +62,8 @@ total_deaths INT,
 total_recovered INT,
 active_cases INT,
 death_permillion INT,
-foreign key(country_code) references country(country_code) );
+foreign key(country_code) references country(country_code)
+);
 
 Create table if not exists medals(
 country_code CHAR(5) primary key,
@@ -62,11 +71,13 @@ Year INT,
 Gold INT,
 Silver INT,
 Broze INT,
-foreign key(country_code) references country(country_code) );
+foreign key(country_code) references country(country_code)
+);
+
 
 Create table if not exists universities(
 Uiversity_id INT primary key auto_increment,
 Country_code CHAR(5),
 Uni_name VARCHAR(100),
 Uni_url VARCHAR(100),
-foreign key(country_code) references country(country_code) );
+foreign key(country_code) references country(country_code));
